813c5d3467cb07cddb2ab6806b6064fd59df883e
J0nWater <79053067+J0nWater@users.noreply.github.com>
2024-08-27 14:26:58 +0200

    bear

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+
+const washing = document.getElementById("wash"); // skriv washing isteden for hele den lange greia.
+const playing = document.getElementById("play"); //     //      //
+const feeding = document.getElementById("feed"); //     //      //
+const txtbx = document.getElementById("textBox");
+
+let time;
+
+function washBear() {
+
+    
+}
+
+function playWithBear() {
+    time = 1000;  // millisecunder hvor 1000 = 1 sekund
+    setInterval(washBear, 60000);
+
+}
+
+function feedBear() {
+
+
+}
+
+function pressButton(washing,playing,deeding){
+
+    document.getElementById('screen').innerHTML = /*html*/`
+    
+    
+    `;
+    
+}
+
